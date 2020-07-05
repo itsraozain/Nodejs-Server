f01ca472532517cc8af3de5f09b6954938ac4662
itsraozain <raozain70@gmail.com>
2020-07-05 10:18:45 +0500

    zain v1

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -1,34 +1,35 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const requireAuth=require('../midlewares/requireAuth');
+const express = require('express');
+const mongoose = require('mongoose');
+const requireAuth = require('../midlewares/requireAuth');
 
-const Track=mongoose.model('Track');
+const Track = mongoose.model('Track');
 
-const router=express.Router();
+const router = express.Router();
 
 router.use(requireAuth);
 
-router.get('/tracks',async(req,res)=>{
-    const tracks=await Track.find({userId:req.user._id})
+router.get('/tracks', async (req, res) => {
+  const tracks = await Track.find({ userId: req.user._id });
 
-    res.send(tracks);
-})
+  res.send(tracks);
+});
 
-router.post('/tracks',async(req,res)=>{
-    const {name,location}=req.body;
-    if(!name || !location){
-        return res.status(422).send({error:'you must provide name and location'})
-    }
-try {
-    const track=new Track({name,location,userId:req.user._id});
+router.post('/tracks', async (req, res) => {
+  const { name, locations } = req.body;
+
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide a name and locations' });
+  }
+
+  try {
+    const track = new Track({ name, locations, userId: req.user._id });
     await track.save();
     res.send(track);
-    
-} catch (err) {
-    return res.status(422).send({error:err.message});
-    
-}
-   
-})
+  } catch (err) {
+    res.status(422).send({ error: err.message });
+  }
+});
 
-module.exports=router;
+module.exports = router;
